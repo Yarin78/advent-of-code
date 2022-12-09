ffe1705446cfd4b5e65b3207ff7025124a22fe93
Jimmy Mårdell <jimmy.mardell@gmail.com>
2022-12-09 06:31:11 +0100

    Day 9 update

--- src/year2022/day9.py
+++ src/year2022/day9.py
@@ -5,18 +5,15 @@ from yal.graph import *
 from yal.geo2d import *
 
 rope = [Point(0,0) for _ in range(10)]
-tail_vis = set([rope[-1]])
+vis = [set() for _ in range(10)]
 
 for line in sys.stdin.readlines():
-    dir = DIRECTION_MAP[line[0]]
-
     for step in range(int(line[2:])):
-        rope[0] += dir
+        rope[0] += DIRECTION_MAP[line[0]]
         for i in range(1, len(rope)):
             diff = rope[i-1]-rope[i]
             if abs(diff.x) == 2 or abs(diff.y) == 2:
                 rope[i] += Point(sign(diff.x), sign(diff.y))
+            vis[i].add(rope[i])
 
-        tail_vis.add(rope[-1])
-
-print(len(tail_vis))
+print(len(vis[1]), len(vis[9]))
