ae30db0d0567042dc1654c48ff918e08ba030d03
Jimmy Mårdell <jimmy.mardell@gmail.com>
2022-12-12 06:39:41 +0100

    Day 12 update

--- src/year2022/day12.py
+++ src/year2022/day12.py
@@ -1,28 +1,12 @@
 from yal.grid import *
 from yal.graph import *
 
-def _is_forward_edge(p1:Point, c1:str, p2:Point, c2:str):
-    return ord(c2)<=ord(c1)+1
-
-def _is_back_edge(p1:Point, c1:str, p2:Point, c2:str):
-    return ord(c1)<=ord(c2)+1
-
-def _is_node(p1:Point, c1:str):
-    return True
-
 grid = Grid.read()
-start = grid.find('S')[0]
-goal = grid.find('E')[0]
 
-grid[start] = 'a'
-grid[goal] = 'z'
+start = grid.find_and_replace('S', 'a')[0]
+goal = grid.find_and_replace('E', 'z')[0]
 
-g = grid_graph(grid, is_node=_is_node, get_edge=_is_forward_edge)
+g = grid_graph(grid, get_edge=lambda p1,c1,p2,c2:ord(c2) <= ord(c1)+1)
 
-part1 = bfs(g, start)
-print(part1[goal])
-
-g = grid_graph(grid, is_node=_is_node, get_edge=_is_back_edge)
-res = bfs(g, goal)
-
-print(min(res[p] for p in res.keys() if grid[p] == 'a'))
+print(bfs(g, start)[goal])
+print(bfs(g, grid.find('a'))[goal])
