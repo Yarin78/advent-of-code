353adcb1b5d60bbabb2c200599d64cb77fd4684f
Jimmy Mårdell <jimmy.mardell@gmail.com>
2024-01-01 20:13:15 +0100

    Day 26 update

--- src/year2023/day26.py
+++ src/year2023/day26.py
@@ -1,12 +1,25 @@
 import sys
+from functools import cache
 from collections import defaultdict
 from queue import Queue
 from typing import Tuple, List, Optional
 
-SHOW_SOLUTIONS = True
+SHOW_SOLUTIONS = False
+STORE_SOLUTIONS = False
+
+# Force cells in a grid. Use '#', '.' or '?'
+FORCED = [
+    # "########",
+    # "#......#",
+    # "#.####.#",
+    # "#.#..#.#",
+    # "#.#.##.#",
+    # "#.#.#..#",
+    # "###.####"
+]
 
 def scanline_merge_components(
-    previous_row: List[Optional[int]], current_row: List[bool]
+    previous_row: Tuple[Optional[int], ...], current_row: List[bool]
 ) -> Tuple[List[Optional[int]], List[int], List[int]]:
     '''
     Helper function for merging components across two rows in a grid.
@@ -52,6 +65,9 @@ def scanline_merge_components(
                 if x+1 < n and current_row[x+1] and not seen[x+1]:
                     q.put(x+1)
                     seen[x+1] = True
+                if x-1 >= 0 and current_row[x-1] and not seen[x-1]:
+                    q.put(x-1)
+                    seen[x-1] = True
                 if previous_row[x] is not None:
                     new_component = False
                     for j in comp_column_map[previous_row[x]]:
@@ -66,35 +82,36 @@ def scanline_merge_components(
 
     return (output, sorted(k for k, v in comp_column_map.items() if v), new_components)
 
+def valid_pattern_bitmask(row1: int, row2: int):
+    # Certain patterns in any local 3x2 part of the grid are invalid
+    # Examples of invalid local patterns:
+    # ...   #..   .##   .#.
+    # .#.   .##   .##   ###
 
-def valid_pattern(row1: List[Optional[int]], row2: List[bool]):
-    s1 = ''.join('#' if comp is not None else '.' for comp in row1)
-    s2 = ''.join('#' if filled else '.' for filled in row2)
-    assert len(s1) == 3 and len(s2) == 3
+    assert row1 < 8 and row2 < 8
 
-    def matches(p1: str, p2: str):
-        n = len(s1)
-        pn = len(p1)
-        for i in range(n - pn + 1):
-            if s1[i:i+pn] == p1 and s2[i:i+pn] == p2:
-                return True
-            if s2[i:i+pn] == p1 and s1[i:i+pn] == p2:
-                return True
+    if row1 in (0, 7) and row2 == 2:
         return False
-
-    if matches('.#', '#.'):
+    if row1 == 2 and row2 in (0, 7):
         return False
-    if matches('##', '##'):
+    if (row1 & 3) == 3 and (row2 & 3) == 3:
         return False
-
-    if matches('###', '.#.'):
+    if (row1 & 6) == 6 and (row2 & 6) == 6:
         return False
-    if matches('...', '.#.'):
+    if (row1 & 3) == 2 and (row2 & 3) == 1:
         return False
-
-
+    if (row1 & 3) == 1 and (row2 & 3) == 2:
+        return False
+    if (row1 & 6) == 2 and (row2 & 6) == 4:
+        return False
+    if (row1 & 6) == 4 and (row2 & 6) == 2:
+        return False
     return True
 
+precalc_valid_patterns = []
+for i in range(8):
+    for j in range(8):
+        precalc_valid_patterns.append(valid_pattern_bitmask(i, j))
 
 def valid_final_row(components: List[Optional[int]], up: List[bool]):
     if 1 in components:
@@ -116,7 +133,9 @@ def valid_final_row(components: List[Optional[int]], up: List[bool]):
 
     return True
 
-def validate_up(row1: List[Optional[int]], row2: List[Optional[int]], up: List[bool]):
+def validate_up(row1: Tuple[Optional[int], ...], row2: List[Optional[int]], up: Tuple[bool, ...]):
+    # Both edges of horizontal lines in row1 must either go up (up[x] set) or down (row2[x] set)
+
     n = len(row1)
     for i in range(n):
         if row1[i] is None:
@@ -132,13 +151,30 @@ def validate_up(row1: List[Optional[int]], row2: List[Optional[int]], up: List[b
     return True
 
 current_grid = []
+all_solutions = []
+
+def rotate(grid: List[str]):
+    res = []
+    xsize, ysize = len(grid[0]), len(grid)
+    for x in range(xsize):
+        s = ""
+        for y in range(ysize):
+            s += grid[ysize -1 - y][x]
+        res.append(s)
+    return res
 
 def show():
     for row in current_grid:
         print(row)
     print()
 
-def count_grid_loops(components: List[Optional[int]], up: List[bool], rows_left: int):
+def store():
+    global all_solutions
+    all_solutions.append(current_grid[:])
+
+
+@cache
+def count_grid_loops_rec(components: Tuple[Optional[int], ...], up: Tuple[bool, ...], rows_left: int, row_num: int):
     if rows_left == 0:
         return 0
 
@@ -149,18 +185,17 @@ def count_grid_loops(components: List[Optional[int]], up: List[bool], rows_left:
     n = len(components)
     current_row = [False] * n
 
-    def rec(x: int):
+    def rec(x: int, row1_last3: int, row2_last3: int):
         nonlocal n, started
-        if x == 2 and not valid_pattern([None, *components[x-2:x]], [False, *current_row[x-2:x]]):
-            return 0
-        if x >= 3 and not valid_pattern(components[x-3:x], current_row[x-3:x]):
+
+        if not precalc_valid_patterns[row1_last3 * 8 + row2_last3]:
             return 0
 
         if x == n:
-            if not valid_pattern([*components[x-2:x], None], [*current_row[x-2:x], False]):
-                 return 0
+            if not precalc_valid_patterns[((row1_last3 * 2) & 7) * 8 + ((row2_last3 * 2) & 7)]:
+                return 0
 
-            if SHOW_SOLUTIONS:
+            if SHOW_SOLUTIONS or STORE_SOLUTIONS:
                 current_grid.append(''.join('#' if filled else '.' for filled in current_row))
 
             merged_components, lost_components, _ = scanline_merge_components(components, current_row)
@@ -169,40 +204,101 @@ def count_grid_loops(components: List[Optional[int]], up: List[bool], rows_left:
                     return 0
 
                 if len(lost_components) > 0:
+                    # Ensure we don't have multiple loops
                     return 0
 
                 new_up = [merged_components[i] is not None and components[i] is not None for i in range(n)]
 
                 if started and valid_final_row(merged_components, new_up):
-                    if SHOW_SOLUTIONS:
+                    if SHOW_SOLUTIONS or STORE_SOLUTIONS:
                         for _ in range(rows_left-1):
                             current_grid.append('.' * n)
-                        show()
+                        if SHOW_SOLUTIONS:
+                            show()
+                        if STORE_SOLUTIONS:
+                            store()
                         for _ in range(rows_left-1):
                             current_grid.pop()
 
                     return 1
 
-                return count_grid_loops(merged_components, new_up, rows_left - 1)
+                return count_grid_loops_rec(tuple(merged_components), tuple(new_up), rows_left - 1, row_num + 1)
             finally:
-                if SHOW_SOLUTIONS:
+                if SHOW_SOLUTIONS or STORE_SOLUTIONS:
                     current_grid.pop()
 
-        current_row[x] = False
-        ans = rec(x+1)
-        current_row[x] = True
-        ans += rec(x+1)
+        mask1 = ((row1_last3 * 2) & 7) + (components[x] is not None)
+        mask2 = (row2_last3 * 2) & 7
+
+        ans = 0
+
+        c = FORCED[row_num][x] if row_num < len(FORCED) and x < len(FORCED[row_num]) else '?'
+
+        if c != '#':
+            current_row[x] = False
+            ans += rec(x+1, mask1, mask2)
+        if c != '.':
+            current_row[x] = True
+            ans += rec(x+1, mask1, mask2+1)
 
         return ans
 
-    return rec(0)
+    return rec(0, 0, 0)
+
+def count_grid_loops(xsize, ysize):
+    return count_grid_loops_rec(tuple([None] * xsize), tuple([False] * xsize), ysize, 0)
+
+def symmetry_compare(N: int, M: int):
+    global all_solutions
+    count_grid_loops(M, N)
+
+    print(f"Found {len(all_solutions)} solutions for {M}x{N} grid")
+
+    rotated_solutions = set()
+    for solution in all_solutions:
+        rotated_solutions.add('\n'.join(rotate(solution)))
+    all_solutions = []
+
+    count_grid_loops(N, M)
+
+    print(f"Found {len(all_solutions)} solutions for {N}x{M} grid")
+
+    for solution in all_solutions:
+        sol = '\n'.join(solution)
+        if sol in rotated_solutions:
+            rotated_solutions.remove(sol)
+        else:
+            print(f"Found in {N}x{M} but not in {M}x{N}")
+            print(sol)
+            print()
+
+    for sol in rotated_solutions:
+        print(f"Found in {M}x{N} but not in {N}x{M}")
+        print(sol)
+
+def main(xsize, ysize):
+    print(f"Counting loops in grid of size {xsize}x{ysize}")
+    num_solutions = count_grid_loops(xsize, ysize)
+    print("Number of solutions:", num_solutions)
+    try:
+        print(count_grid_loops_rec.cache_info())
+    except:
+        pass
+
+    # 6x5         571
+    # 6x6        2685
+    # 7x5        1656
+    # 7x6       10314
+    # 7x7       50391
+    # 8x7      222805
+    # 8x8     1188935
+    # 8x9     6510243
+    # 8x10   39576571
+    # 9x9    41749885
+    # 9x10  303385827
+    # 8x20  3350776906928379
 
 
-XSIZE = int(sys.argv[1])
-YSIZE = int(sys.argv[2])
+# symmetry_compare(7, 8)
 
-print(f"Counting loops in grid of size {XSIZE}x{YSIZE}")
-
-num_solutions = count_grid_loops([None] * XSIZE, [False] * XSIZE, YSIZE)
-
-print("Number of solutions:", num_solutions)
+main(int(sys.argv[1]), int(sys.argv[2]))
